0014a048402d3c67347dfc1edeed4ee286d6b11a
Matt Dassow <dassowmd@gmail.com>
2025-09-20 15:17:09 -0500

    reviewing

--- src/neon_crm/resources/campaigns.py
+++ src/neon_crm/resources/campaigns.py
@@ -2,13 +2,13 @@
 
 from typing import TYPE_CHECKING, Any, Dict, Iterator, Optional
 
-from .base import BaseResource
+from .base import ListableResource
 
 if TYPE_CHECKING:
     from ..client import NeonClient
 
 
-class CampaignsResource(BaseResource):
+class CampaignsResource(ListableResource):
     """Resource for managing campaigns."""
 
     def __init__(self, client: "NeonClient") -> None:
