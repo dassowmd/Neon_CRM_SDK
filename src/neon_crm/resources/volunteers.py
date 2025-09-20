ccea0be36ec93f53d815f046c99f86666d240b8d
Matt Dassow <dassowmd@gmail.com>
2025-09-20 15:19:02 -0500

    reviewing

--- src/neon_crm/resources/volunteers.py
+++ src/neon_crm/resources/volunteers.py
@@ -2,13 +2,13 @@
 
 from typing import TYPE_CHECKING, Any, Dict, Iterator, Optional
 
-from .base import BaseResource
+from .base import ListableResource
 
 if TYPE_CHECKING:
     from ..client import NeonClient
 
 
-class VolunteersResource(BaseResource):
+class VolunteersResource(ListableResource):
     """Resource for managing volunteers."""
 
     def __init__(self, client: "NeonClient") -> None:
